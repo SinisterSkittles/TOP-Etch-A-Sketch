7aea34adff1208060194000e22a006906399d341
Fatin <abdelrahimfa@gmail.com>
2025-10-02 10:45:33 -0500

    Add a 16X16 grid

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let gridContainer = document.querySelector("#grid");
+const gridContainer = document.querySelector("#grid");
+
 
 function generateGrid(gridSize) {
     for(let i = 0; i < gridSize; i++) {
         let temp = document.createElement("div");
         temp.setAttribute("style", "border: 2px solid blue;");
+        temp.setAttribute("class", "hoverDiv");
         gridContainer.appendChild(temp);
     }
 }
 
+const hoverDivs = document.querySelectorAll(".hoverDiv");
+
 //document.createElement("div").setAttribute("style", "border: 2px solid blue;")
 generateGrid(16*16);
+
